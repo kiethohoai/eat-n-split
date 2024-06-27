8539f9a2efdfec5948c0c90f2f26fcba5bfe0937
kiethohoai <kietho.060193@gmail.com>
2024-06-27 17:22:17 +0700

    003 Building the Static App List of Friends

---
+++ README.md
@@ -0,0 +1,3 @@
+001 Section Overview
+002 Project Setup
+003 Building the Static App List of Friends
--- src/App.js
+++ src/App.js
@@ -19,6 +19,48 @@ const initialFriends = [
   },
 ];
 
+// App
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+// FriendsList
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+// Friend
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <button className="button">Select</button>
+    </li>
+  );
 }
